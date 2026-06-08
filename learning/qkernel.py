ba1ba8c6b260b88f72ed63ec25383b30fa9a5c3e
oxw <oxw@163.com>
2026-06-08 10:06:07 +0000

    add nvidia-mqpu testing code segment

--- learning/qkernel.py
+++ learning/qkernel.py
@@ -34,3 +34,33 @@ most_probable_result = results.most_probable()
 probability = results.probability(most_probable_result)
 print("Most probable result: " + most_probable_result)
 print("Measured with probability: " + str(probability), end="\n\n")
+
+
+### nvidia-mqpu testing
+
+@cudaq.kernel
+def kernel2(qubit_count: int):
+    # Allocate our qubits.
+    qvector = cudaq.qvector(qubit_count)
+    # Place all qubits in a uniform superposition.
+    h(qvector)
+    # Measure the qubits.
+    mz(qvector)
+    
+num_gpus = cudaq.num_available_gpus()
+print(f"Number of available GPUs: {num_gpus}")
+
+if num_gpus > 1:
+    # Set the target to include multiple virtual QPUs.
+    cudaq.set_target("nvidia", option="mqpu")
+    # Asynchronous execution on multiple virtual QPUs, 
+    # each simulated by an NVIDIA GPU.
+    result_1 = cudaq.sample_async(kernel, qubit_count, shots_count=10000, qpu_id=0)
+    result_2 = cudaq.sample_async(kernel2, qubit_count, shots_count=10000, qpu_id=1)
+else:
+    # Schedule for execution on the same vritual QPU.
+    result_1 = cudaq.sample_async(kernel, qubit_count, shots_count=10000, qpu_id=0)
+    result_2 = cudaq.sample_async(kernel2, qubit_count, shots_count=10000, qpu_id=0)
+    
+print("Measurement distribution for kernel: " + str(result_1.get()))
+print("Measurement distribution for kernel2: " + str(result_2.get()))
